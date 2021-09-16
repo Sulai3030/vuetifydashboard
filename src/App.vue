89bb45f9db80959daabf4ff7eb57fce49b50fa25
sssulai@gmail.com <you@example.com>
2021-09-16 17:30:26 -0400

    added content to Login and Dashbpard views

--- src/App.vue
+++ src/App.vue
@@ -16,32 +16,6 @@
     </v-app-bar>
     <v-content></v-content>
     <v-spacer></v-spacer>
-    <v-card width="400" class="mx-auto mt-5">
-      <v-card-title>
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="User"></v-text-field>
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="mdi-lock"
-              append-icon="showPassword ? 'mdi-eye-off'
-              'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            ></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="success">Login</v-btn>
-        </v-card-actions>
-      </v-card-title>
-    </v-card>
     <router-view></router-view>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,14 +5,26 @@
       :items="desserts"
       :items-per-page="5"
       class="elevation-1"
-    ></v-data-table
-    >>
+    ></v-data-table>
+    <div class="text-center ma-2">
+      <v-btn dark @click="snackbar = true"> Open Snackbar </v-btn>
+      <v-snackbar v-model="snackbar">
+        {{ text }}
+
+        <template v-slot:action="{ attrs }">
+          <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
+      snackbar: true,
       headers: [
         {
           text: "Dessert (100g serving)",
